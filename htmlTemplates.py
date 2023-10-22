d3e2a5d2ffba69eb016f5e8685cbf21c4f967ffc
MUmarJ <umarjh96@gmail.com>
2023-10-22 13:45:43 -0400

    Run formatter on htmlTemplates

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -1,4 +1,4 @@
-css = '''
+css = """
 <style>
 .chat-message {
     padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
@@ -23,22 +23,22 @@ css = '''
   padding: 0 1.5rem;
   color: #fff;
 }
-'''
+"""
 
-bot_template = '''
+bot_template = """
 <div class="chat-message bot">
     <div class="avatar">
         <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
-'''
+"""
 
-user_template = '''
+user_template = """
 <div class="chat-message user">
     <div class="avatar">
         <img src="https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/e4/af/e4afdbe6-59c0-4a6b-8112-8b30e274d9a0/2009-49519-h-floresiensis-jgurche_web.jpg">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
-'''
+"""
